500f18c4e5e674e95e07d831ff7e7ef8f109a761
HoCG <rhwkd5012@naver.com>
2021-09-20 15:31:16 +0900

    수정사항

--- src/App.vue
+++ src/App.vue
@@ -2,30 +2,8 @@
     <v-card id="app" class="mx-auto" max-width="500">
         <v-toolbar color="teal" dark="dark">
             <v-app-bar-nav-icon></v-app-bar-nav-icon>
-
             <v-toolbar-title>Topics</v-toolbar-title>
-
             <v-spacer></v-spacer>
-            <!--<v-list>
-                <v-list-group
-                    v-for="item in dept"
-                    :key="item.title"
-                    v-model="item.active"
-                    :prepend-icon="item.action"
-                    no-action="no-action">
-                    <template v-slot:activator="v-slot:activator">
-                        <v-list-item-content>
-                            <v-list-item-title v-text="item.title"></v-list-item-title>
-                        </v-list-item-content>
-                    </template>
-
-                    <v-list-item v-for="child in item.dept" :key="child.title">
-                        <v-list-item-content>
-                            <v-list-item-title v-text="child.title"></v-list-item-title>
-                        </v-list-item-content>
-                    </v-list-item>
-                </v-list-group>
-            </v-list>-->
             <v-btn icon="icon">
                 <v-icon>mdi-dots-vertical</v-icon>
             </v-btn>
@@ -54,5 +32,8 @@
         text-align: center;
         color: #2c3e50;
         margin-top: 60px;
+    }
+    i{
+        color: black !important;
     }
 </style>
--- src/components/NodeTree.vue
+++ src/components/NodeTree.vue
@@ -6,6 +6,7 @@
                     <template v-slot:activator>
                         <v-list-item-content>
                             <v-list-item-icon>
+                                &nbsp;&nbsp;&nbsp;&nbsp;
                                 <v-icon>mdi-home</v-icon>
                                 <v-list-item-title v-text="item.title"></v-list-item-title>
                             </v-list-item-icon>
@@ -15,7 +16,16 @@
                 </v-list-group>
             </v-list>
             <!--최하위값들-->
-            <v-list v-else>{{item.title}}</v-list>
+            <v-list v-else>
+                <v-list-item-content>
+                    <v-list-item-icon>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                        <v-icon class="EndIcon">mdi-star</v-icon>
+                        <v-list-item-title v-text="item.title"></v-list-item-title>
+                        <!--v-expansion-panel을 여기에 적용시키는것을 고려해보자.-->
+                    </v-list-item-icon>
+                </v-list-item-content>
+            </v-list>
         </v-list>
     </v-list>
 </template>
@@ -24,7 +34,10 @@
     export default {name: "mynode", props: ["treeData"]};
 </script>
 <style>
-.v-list-item__title{
-    float: left;
-}
+    .v-list-item__title {
+        float: left;
+    }
+    .EndIcon{
+        color: yellow !important;
+    }
 </style>
